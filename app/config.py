d2671f9deee095fcf7db8d7149359d2d0e88cb50
Rishav-R03 <rishav042023@gmail.com>
2025-07-20 12:05:40 +0530

    updated config file redis

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     DATABASE_URL: str = "postgresql+asyncpg://postgres:root123@localhost:5432/url_shortener_db"
-    REDIS_HOST: str = "redis-19852.c17.us-east-1-4.ec2.redns.redis-cloud.com"
-    REDIS_PORT: int = 19852
+    REDIS_HOST: str = "172.25.41.139"
+    REDIS_PORT: int = 6379
     REDIS_DB: int = 0
     SHORT_CODE_LENGTH: int = 8
     RATE_LIMIT_PER_MINUTE: int = 10
